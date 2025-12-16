7d66d586ff51b46ecb338a04035f30d771b3f1b8
Nischal213 <xxwassupboixx@gmail.com>
2025-12-16 22:28:06 +0000

    Made a navbar in tailwind with no functionality

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -2,19 +2,22 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="justify-center flex gap-2 bg-white max-w-lg">
-      <div className="bg-yellow-200">
-        <p className="text-black">About Us</p>
-      </div>
-      <div>
-        <p>Contact Us</p>
-      </div>
-      <div>
-        <p>Donate</p>
-      </div>
-      <div>
-        <p>Learn more</p>
-      </div>
+    <div className="justify-center flex gap-7 bg-[#1f1f1f] max-w-md m-auto mt-10 py-2 rounded-3xl text-white text-lg shadow-lg/50">
+        <div className="bg-white rounded-full max-w-8">
+          <img src={"saturn.png"} className="p-1"></img>
+        </div>
+        <div className="mt-0.5">
+          <p> Home </p>
+        </div>
+        <div className="mt-0.5">
+          <p> About us </p>
+        </div>
+        <div className="mt-0.5">
+          <p> FAQ </p>
+        </div>
+        <div className="mt-0.5">
+          <p> Contact us </p>
+        </div>
     </div>
   );
 }
